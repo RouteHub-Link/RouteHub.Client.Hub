836d4d8cdb5dc8c4d13ba84f4d93b5ef66bba9c7
guneskorkmaz <guneskorkmaz@outlook.com.tr>
2024-08-12 23:22:07 +0300

    feature : make pin mock more functional

--- server/router/handlers/pin_handler.go
+++ server/router/handlers/pin_handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/RouteHub-Link/routehub.client.hub/server/extensions"
 	"github.com/RouteHub-Link/routehub.client.hub/templates/layouts/components"
@@ -12,17 +13,15 @@ import (
 )
 
 func (eh echoHandlers) PinHandler(c echo.Context) error {
-	// get random int between 324 to 48129
+	pins := getMockData()
 
-	rows := []components.TableRow{{Cells: []components.TableCell{
-		{Content: "<a href='https://routehub.link' target='_blank'> Rotehub.Link Home </a>", Class: ""}, {Content: randomIntAsString(300, 3000), Class: ""}}, Class: ""},
-		{Cells: []components.TableCell{
-			{Content: "<a href='https://routehub.link' target='_blank'> Rotehub.Test </a>", Class: ""}, {Content: randomIntAsString(300, 3000), Class: ""}}, Class: ""},
-		{Cells: []components.TableCell{
-			{Content: "<a href='https://routehub.link' target='_blank'> Rotehub.Test2 </a>", Class: ""}, {Content: randomIntAsString(300, 3000), Class: ""}}, Class: ""},
-	}
+	return extensions.Render(c, http.StatusOK, pages.Pins(eh.layoutDescription, pins))
+}
 
-	headers := []components.TableHeader{{Content: "URL", Class: ""}, {Content: "Clicks", Class: ""}}
+func getMockData() []components.PanelDescription {
+	rows := randomRows(15)
+	rows2 := randomRows(12)
+	headers := headers()
 
 	td1 := components.TableDescription{
 		Headers:              headers,
@@ -37,17 +36,19 @@ func (eh echoHandlers) PinHandler(c echo.Context) error {
 		Footers:              nil,
 		FooterIsSameAsHeader: true,
 		Class:                "is-bordered is-striped is-narrow is-hoverable is-fullwidth",
-		Rows:                 &rows,
+		Rows:                 &rows2,
 	}
 
 	table1 := components.Table(td1)
 	table2 := components.Table(td2)
 
-	pinPanels := []components.PanelDescription{
+	return []components.PanelDescription{
 		{PanelHeading: "Top Clicks", PanelColorClass: "is-primary", PanelTable: table1},
 		{PanelHeading: "Our Picks", PanelColorClass: "is-info", PanelTable: table2}}
+}
 
-	return extensions.Render(c, http.StatusOK, pages.Pins(eh.layoutDescription, pinPanels))
+func headers() []components.TableHeader {
+	return []components.TableHeader{{Content: "URL", Class: ""}, {Content: "Clicks", Class: ""}}
 }
 
 func randomInt(min, max int) int {
@@ -56,4 +57,30 @@ func randomInt(min, max int) int {
 
 func randomIntAsString(min, max int) string {
 	return strconv.Itoa(randomInt(min, max))
+}
+
+func randomRows(count int) []components.TableRow {
+	rows := make([]components.TableRow, count)
+	for i := 0; i < count; i++ {
+		rows[i] = components.TableRow{Cells: randomTableCells(), Class: ""}
+	}
+	return rows
+}
+
+func randomTableCells() []components.TableCell {
+	randomString := uniqueRandomString(10)
+	count := randomIntAsString(300, 3000)
+	link := strings.Join([]string{"<a href='https://", randomString, ".link' target='_blank'> Rotehub.Link ", randomString, "</a>"}, "")
+
+	return []components.TableCell{
+		{Content: link, Class: ""}, {Content: count, Class: ""}}
+}
+
+func uniqueRandomString(count int) string {
+	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	b := make([]byte, count)
+	for i := range b {
+		b[i] = charset[randomInt(0, len(charset))]
+	}
+	return string(b)
 }
