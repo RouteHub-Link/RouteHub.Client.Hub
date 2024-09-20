7c177ccf9a9456ebc6ea6d1f13be596c44c3343c
guneskorkmaz <guneskorkmaz@outlook.com.tr>
2024-09-20 16:07:28 +0300

    feat : improve random function

--- packages/cusrand/random_string.go
+++ packages/cusrand/random_string.go
@@ -1,10 +1,19 @@
 package cusrand
 
+import (
+	"crypto/rand"
+	"math/big"
+)
+
 func UniqueRandomString(count int) string {
 	const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 	b := make([]byte, count)
 	for i := range b {
-		b[i] = charset[RandomInt(0, len(charset))]
+		num, err := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		if err != nil {
+			panic(err) // handle error appropriately in real applications
+		}
+		b[i] = charset[num.Int64()]
 	}
 	return string(b)
 }
