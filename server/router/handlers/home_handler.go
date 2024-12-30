386e681cf6069f45666f0d3872b66f380669b09d
Runaho <guneskorkmaz@outlook.com.tr>
2024-12-30 23:13:39 +0300

    feature : attach creates home page

--- server/router/handlers/home_handler.go
+++ server/router/handlers/home_handler.go
@@ -3,9 +3,11 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/RouteHub-Link/routehub.client.hub/packages/status"
 	"github.com/RouteHub-Link/routehub.client.hub/server/context"
 	"github.com/RouteHub-Link/routehub.client.hub/server/extensions"
 	"github.com/RouteHub-Link/routehub.client.hub/templates/pages"
+	redirection_pages "github.com/RouteHub-Link/routehub.client.hub/templates/pages/redirections"
 	"github.com/labstack/echo/v4"
 )
 
@@ -24,5 +26,16 @@ func (eh echoHandlers) HomeHandler(c echo.Context) error {
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
 
-	return extensions.Render(c, http.StatusOK, pages.Home(*platform.LayoutDescription))
+	lcs := sec.GetLinkClientService()
+
+	link, err := lcs.GetLink(ctx, "home")
+	if err != nil || link == nil {
+		return extensions.Render(c, http.StatusOK, pages.Home(*platform.LayoutDescription))
+	}
+
+	if link.Status == status.StatusInactive {
+		return echo.NewHTTPError(http.StatusNotFound, "Link is not active")
+	}
+
+	return extensions.Render(c, http.StatusOK, redirection_pages.Custom(*platform.LayoutDescription, *link))
 }
